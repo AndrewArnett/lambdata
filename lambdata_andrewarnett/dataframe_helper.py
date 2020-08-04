0628b3e35f504f95a245aade7c918f026dcd54e5
Andrew Arnett <andrewarnett2323@gmail.com>
2020-08-04 12:50:46 -0700

    adding work to github

--- lambdata_andrewarnett/dataframe_helper.py
+++ lambdata_andrewarnett/dataframe_helper.py
@@ -4,6 +4,13 @@ Some functions to help cleaning and handling dataframes.
 
 import pandas as pd
 
-def report_missing_values(df):
-    """Print a pretty report of missing values."""
-    print("Need to write code here!")
+def shape_head(df):
+    """Returns shape and first 5 rows of df."""
+    shape = print(df.shape)
+    head = df.head()
+    return shape, head
+
+def baseline(target):
+    """calculates baseline of target """
+    y = df[target]
+    return y.value_counts(normalize=True)
